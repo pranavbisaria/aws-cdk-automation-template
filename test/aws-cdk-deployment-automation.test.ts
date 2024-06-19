7f7b7c6b59aef7806ce37db71589afaa371ac083
Pranav Bisaria <pranavbisariya29@gmail.com>
2024-06-19 21:33:41 +0530

    Initial commit

---
+++ test/aws-cdk-deployment-automation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkDeploymentAutomation from '../lib/aws-cdk-deployment-automation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-deployment-automation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkDeploymentAutomation.AwsCdkDeploymentAutomationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
